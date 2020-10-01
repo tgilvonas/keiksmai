1769f6190bf3f77dc17881b05a04e28ececee031
Tomas Gilvonauskas <tomas@artme.lt>
2020-10-01 21:29:20 +0300

    Initial data models were set

--- keiksmai_frontend/models.py
+++ keiksmai_frontend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Site_user(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    show_only_nickname = models.BooleanField()
+    created_at = models.DateTimeField('Created at')
+
+
+class Swearing(models.Model):
+    swearing = models.CharField(max_length=255)
+    language = models.CharField(max_length=3)
+    confirmed = models.BooleanField()
+    user = models.ForeignKey(Site_user, models.SET_NULL,
+    	blank=True,
+    	null=True,)
+    created_at = models.DateTimeField('Created at')
+
